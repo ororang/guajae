2324b7687b5bc805eb5e5cff6afa4c2409aea47d
ororang <102219474+ororang@users.noreply.github.com>
2025-10-13 10:03:28 +0900

    Update car_buy.py

--- car_buy.py
+++ car_buy.py
@@ -1,1 +1,26 @@
+print("예산은 2000만원입니다")
+print("후보를 정해봅시다")
 
+
+while(True):
+    car_num = int(input("1. 기아 모닝 2. 기아 k5 3.기아 스포티지 4.기아 카니발"))
+    selection = input("이 차로 고르시겠습니까? yes/no")
+
+
+    if(selection = "yes"):
+        print("구매방식을 고르겠습니다")
+        buybuy = int(input("1. 신차 구매 2. 중고차 구매 3. 랜트"))
+        if(buybuy > 0 and buybuy < 4):
+            buy_selection = input("이 방식으로 구매하시시겠습니까? yes/no")
+            if(buy_selection == "yes"):
+                print("구매 완료했습니다")
+                break
+            else:
+                continue
+        else:
+            print("invaild Input")
+    if(selection != "no"):
+        print("Invailt Input")
+    else:
+        continue
+      
