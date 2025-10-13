67f8852cb84d70d8780127da1d8febd99682cde1
ororang <102219474+ororang@users.noreply.github.com>
2025-10-13 10:05:59 +0900

    Update car_buy.py

--- car_buy.py
+++ car_buy.py
@@ -4,10 +4,12 @@ print("후보를 정해봅시다")
 
 while(True):
     car_num = int(input("1. 기아 모닝 2. 기아 k5 3.기아 스포티지 4.기아 카니발"))
+    if(car_num > 4 and car_num < 1):
+        print("Invalid Input")
     selection = input("이 차로 고르시겠습니까? yes/no")
 
 
-    if(selection = "yes"):
+    if(selection == "yes"):
         print("구매방식을 고르겠습니다")
         buybuy = int(input("1. 신차 구매 2. 중고차 구매 3. 랜트"))
         if(buybuy > 0 and buybuy < 4):
@@ -23,4 +25,3 @@ while(True):
         print("Invailt Input")
     else:
         continue
-      
